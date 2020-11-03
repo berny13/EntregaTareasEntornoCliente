ae7d44e15111e21d8713878f220f41c3fff3c011
berny13 <bernyjimenezsanchez@gmail.com>
2020-11-03 15:44:05 +0100

    se eliminan carpetas repetidas

--- ObjetosPredefinidos/Date/LecheYGalletas.js
+++
@@ -1,3 +0,0 @@
-function lecheCookies(noche) {
-  return noche.getDate() == 24 && noche.getMonth() == 12;
-}
--- ObjetosPredefinidos/Date/diasDeLaSemana.js
+++
@@ -1,5 +0,0 @@
-function getDay(diasSemana) {
-  var diasSemana = [ 'lunes', 'martes', 'miercoles', 'jueves', 'viernes', 'sabado', 'domingo'];
-  var semana = new Date(diasSemana);
-  return diasSemana[semana.getDay()];
-}
