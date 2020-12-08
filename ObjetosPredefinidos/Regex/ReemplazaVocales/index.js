716684f47e62ba4439683832e13451e8bd46940c
berny13 <57494751+berny13@users.noreply.github.com>
2020-12-08 12:03:21 +0100

    se carga funcion

---
+++ ObjetosPredefinidos/Regex/ReemplazaVocales/index.js
@@ -0,0 +1,8 @@
+function remplazaCalvo(str, ch) {
+	const vowels = ['a', 'e', 'i', 'o', 'u'];
+	
+	const strArry = [...str].map((char) => {
+		return vowels.includes(char) ? ch : char;
+	});
+	return strArry.join('');
+}
